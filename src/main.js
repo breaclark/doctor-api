d435b600f4affb89397e135861f72649bac7dc7a
Brea Borlas <breatortilla@gmail.com>
2018-04-27 08:41:02 -0700

    Import apiCaller and call from main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
-// import $ from 'jquery';
+import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
+import { apiCaller } from './api.js'
+
+
+$(document).ready(function() {
+  apiCaller();
+});
