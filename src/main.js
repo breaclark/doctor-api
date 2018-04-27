ad0a5549e16585361ce6872404e0f8f4a19cabd2
Brea Borlas <breatortilla@gmail.com>
2018-04-27 09:53:49 -0700

    Add functionality for return of empty list

--- src/main.js
+++ src/main.js
@@ -5,26 +5,30 @@ import './styles.css';
 import { GetData } from './api.js'
 
 let displayData = function (doctors) {
-  $("#result").empty();
-  for (let i = 0; i < doctors.data.length; i ++) {
-    $("#result").append(
-    `<ul>
-      <li>
-        ${doctors.data[i].profile.first_name} ${doctors.data[i].profile.last_name}
-      </li>
-      <li>
-        ${doctors.data[i].practices[0].visit_address.street}    ${doctors.data[i].practices[0].visit_address.city}, ${doctors.data[i].practices[0].visit_address.state}
-      </li>
-      <li>
-        ${doctors.data[i].practices[0].phones[0].number}
-      </li>
-      <li>
-        ${doctors.data[i].practices[0].website}//if undefined put something else here
-      </li>
-      <li>
-        ${doctors.data[i].practices[0].accepts_new_patients}
-      </li>
-    </ul>`);
+  if (doctors.data.length > 0) {
+    $("#result").empty();
+    for (let i = 0; i < doctors.data.length; i ++) {
+      $("#result").append(
+      `<ul>
+        <li>
+          ${doctors.data[i].profile.first_name} ${doctors.data[i].profile.last_name}
+        </li>
+        <li>
+          ${doctors.data[i].practices[0].visit_address.street}    ${doctors.data[i].practices[0].visit_address.city}, ${doctors.data[i].practices[0].visit_address.state}
+        </li>
+        <li>
+          ${doctors.data[i].practices[0].phones[0].number}
+        </li>
+        <li>
+          ${doctors.data[i].practices[0].website}//if undefined put something else here
+        </li>
+        <li>
+          ${doctors.data[i].practices[0].accepts_new_patients}
+        </li>
+      </ul>`);
+    }
+  } else {
+    $("#result").append(`<p>No doctors matched that search, please try again</p>`);
   }
 }
 
