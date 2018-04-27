bb692ee138c227d2b59f2152f041308574022000
Brea Borlas <breatortilla@gmail.com>
2018-04-27 09:02:08 -0700

    Make api call with .env file reference

--- src/api.js
+++ src/api.js
@@ -1,7 +1,9 @@
 import $ from 'jquery';
 
 export function apiCaller() {
-  $.get(`https://api.betterdoctor.com/2016-03-01/doctors?location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${process.env.exports.apiKey}`).then(function(response ) {
+  $.get(`https://api.betterdoctor.com/2016-03-01/doctors?name=michael&location=45.5231%2C-122.6765%2C10&user_location=45.5231%2C-122.6765&gender=male&skip=0&limit=10&user_key=${process.env.exports.apiKey}`).then(function(response ) {
     console.log(response);
+  }).fail(function(error) {
+    console.log(error);
   })
 }
