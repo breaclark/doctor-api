216e2727acc1eac1de4426baeb81e30d66ee6d8b
Brea Borlas <breatortilla@gmail.com>
2018-04-27 09:10:02 -0700

    Add displayData function to front end to display response data

--- src/main.js
+++ src/main.js
@@ -2,9 +2,14 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
-import { apiCaller } from './api.js'
+import { GetData } from './api.js'
 
+let displayData = function (doctors) {
+  $().empty();
+  $().text(doctors);
+}
 
 $(document).ready(function() {
-  apiCaller();
+  let getData = new GetData();
+  getData.apiCaller(displayData);
 });
