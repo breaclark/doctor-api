ff10720fb9f1378b85e2e9232f130b7863967435
Brea Borlas <breatortilla@gmail.com>
2018-04-27 08:46:17 -0700

    Install dotenv-webpack

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
-import $ from "jquery";
+import $ from 'jquery';
 
 export function apiCaller() {
-  $.get(`https://api.betterdoctor.com/2016-03-01/doctors?location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=5af53c2504d057fc1cabd7e95683bbbb`).then(function(response) {
+  $.get(`https://api.betterdoctor.com/2016-03-01/doctors?location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${process.env.exports.apiKey}`).then(function(response ) {
     console.log(response);
   })
 }
