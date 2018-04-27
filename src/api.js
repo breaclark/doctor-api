631bbde511289832b6c2ee8bc4d13b89f5104202
Brea Borlas <breatortilla@gmail.com>
2018-04-27 09:05:39 -0700

    Refactor apiCaller into a class

--- src/api.js
+++ src/api.js
@@ -1,9 +1,15 @@
 import $ from 'jquery';
 
-export function apiCaller() {
-  $.get(`https://api.betterdoctor.com/2016-03-01/doctors?name=michael&location=45.5231%2C-122.6765%2C10&user_location=45.5231%2C-122.6765&gender=male&skip=0&limit=10&user_key=${process.env.exports.apiKey}`).then(function(response ) {
-    console.log(response);
-  }).fail(function(error) {
-    console.log(error);
-  })
+
+class GetData {
+
+  apiCaller() {
+    $.get(`https://api.betterdoctor.com/2016-03-01/doctors?name=michael&location=45.5231%2C-122.6765%2C10&user_location=45.5231%2C-122.6765&gender=male&skip=0&limit=10&user_key=${process.env.exports.apiKey}`).then(function(response ) {
+      console.log(response);
+    }).fail(function(error) {
+      console.log(error);
+    })
+  }
 }
+
+export { GetData };
